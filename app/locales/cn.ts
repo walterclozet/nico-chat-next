1be67d7940d6ad388d95fad039240fb6e1d5626b
walterclozet <walterclozet@gmail.com>
2023-12-31 11:39:47 +0800

    update bot hello cn

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -359,7 +359,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "~にこにこー♪让你的心绽放笑容~我是传递笑容的矢泽妮可~要记得妮可哦♪Love Nico！",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
